c68e7146e61f893c46b9c03bb5452a874ea4f9f2
iangonzalez <gonzalez.isv@gmail.com>
2016-05-06 20:50:55 +0100

    corrected training and test set formats

--- wrangling_and_cleaning/create_test_train_sets.py
+++ wrangling_and_cleaning/create_test_train_sets.py
@@ -3,14 +3,16 @@ import sys
 import pandas as pan
 
 if __name__ == '__main__':
-    data = pan.read_csv(sys.argv[1])
+    data = pan.read_csv(sys.argv[1], nrows = 1200)
 
     data_to_use = data.sample(n = 1200)
 
-    train_data = data_to_use.iloc[:1000, :]
-    test_data = data_to_use.iloc[1000:1100, :]
-    dev_data = data_to_use.iloc[1100:, :]
+    train_data = data_to_use.iloc[:1000, 1:]
+    test_data = data_to_use.iloc[1000:1100, 1:]
+    dev_data = data_to_use.iloc[1100:, 1:]
 
-    train_data.to_csv("data/train.csv", index_label = False)
-    test_data.to_csv("data/test.csv", index_label = False)
-    dev_data.to_csv("data/dev.csv", index_label = False)
+    print(test_data)
+
+    train_data.to_csv("data/train.csv")
+    test_data.to_csv("data/test.csv")
+    dev_data.to_csv("data/dev.csv")
